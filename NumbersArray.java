34b15fb9a6ab37dc96ccbef1c1d25ae8d3c9fea3
andrel293 <andrelquoid.2025@computing.smu.edu.sg>
2026-09-06 18:53:20 +0800

    Q1

--- NumbersArray.java
+++ NumbersArray.java
@@ -1,3 +1,4 @@
+import java.lang.*;
 import java.util.*;
 
 public class NumbersArray {
@@ -20,6 +21,77 @@ public class NumbersArray {
     }
 
     // Write your methods here
-    
-}
+    public static int findMax(Integer[] arr) {
+        int m = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > m){
+                m = arr[i];
+            }
+        }
 
+        return m;
+    }
+
+    public static Integer[] findDuplicates(Integer[] arr){
+
+        List<Integer> dupes = new ArrayList<>();
+
+        Arrays.sort(arr);
+
+        for (int i = 1; i < arr.length; i++){
+            int cur = arr[i];
+
+            if (cur == arr[i-1]){
+
+                boolean in = false;
+
+                for (int j = 0; j < dupes.size(); j++){
+
+                    if (dupes.get(j) == cur){
+                        in = true;
+                    }
+                }
+
+                if (!in) {
+                    dupes.add(cur);
+                }
+
+            }
+
+        }
+
+        return dupes.toArray(Integer[]::new);
+        
+    }
+
+    public static Integer[] findUnique(Integer[] arr){
+
+        Integer[] dupes = findDuplicates(arr);
+
+        Arrays.sort(arr);
+
+        List<Integer> unique = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++){
+
+            int cur = arr[i];
+
+            boolean duplicate = false;
+
+            for (int j = 0; j < dupes.length; j++){
+                if (cur == dupes[j]){
+                    duplicate = true;
+                }
+            }
+
+            if (!duplicate){
+               unique.add(cur);
+            }
+
+        }
+        
+        return unique.toArray(Integer[]::new);
+
+    }
+
+}
